3de97d9dd6a684ac35113fa3b70f474efc773664
a-stanisz <hello@adamstaniszewski.com>
2020-01-10 12:54:07 +0100

    Refactor install script

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+# DIRS
+ALIASES_DIR=''$HOME'/.bash_aliases'
+LOG_DIR=''$HOME'/env-setup.log'
+
 # Timestamp
-echo date >> ~/env-setup.log
+echo `$date` >> $LOG_DIR
 
 # Install neofetch, a cli system info tool:
 sudo apt-get install neofetch
 
 # Add some custom aliases
-touch $HOME/.bash_aliases
-echo "alias os='neofetch'" >> $HOME/.bash_aliases
-echo "alias c='clear'" >> $HOME/.bash_aliases
-echo "alias rld='source ~/.bashrc'" >> $HOME/.bash_aliases
-echo "alias e='exit'" >> $HOME/.bash_aliases
-echo "alias dev='cd ~/workspace; pwd; ls -a'" >> $HOME/.bash_aliases
-echo "alias gs='git status'" >> $HOME/.bash_aliases
-echo "alias python='python3'" >> $HOME/.bash_aliases
-echo "alias ptn='python'" >> $HOME/.bash_aliases
+echo "alias os='neofetch'" >> $ALIASES_DIR
+echo "alias c='clear'" >> $ALIASES_DIR
+echo "alias rld='source ~/.bashrc'" >> $ALIASES_DIR
+echo "alias e='exit'" >> $ALIASES_DIR
+echo "alias dev='cd ~/workspace; pwd; ls -a'" >> $ALIASES_DIR
+echo "alias gs='git status'" >> $ALIASES_DIR
+echo "alias python='python3'" >> $ALIASES_DIR
+echo "alias ptn='python'" >> $ALIASES_DIR
 
 # Summary
-me=`basename "$0"`
-echo 'This is the log file echoed from env-setup script ('$0').' >> ~/env-setup.log
-echo 'Custom aliases added into .bash_aliases file.' >> ~/env-setup.log
-echo 'Installed: neofetch for cli system info.' >> ~/env-setup.log
+echo 'This is the log file echoed from env-setup script ('$0').' >> $LOG_DIR
+echo 'Custom aliases added into .bash_aliases file.' >> $LOG_DIR
+echo 'Installed: neofetch for cli system info.' >> $LOG_DIR
