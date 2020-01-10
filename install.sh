27bff1a5777dbb3e2e29105c2a911640c7e2d2a7
a-stanisz <hello@adamstaniszewski.com>
2020-01-10 12:25:56 +0100

    Change install script

--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo 'This is the log of dotfiles install script' >> ~/dotfiles.log
+# Timestamp
+echo date >> ~/env-setup.log
+
+# Install neofetch, a cli system info tool:
+sudo apt-get install neofetch
+
+# Add some custom aliases
+touch $HOME/.bash_aliases
+echo "alias os='neofetch'" >> $HOME/.bash_aliases
+echo "alias c='clear'" >> $HOME/.bash_aliases
+echo "alias rld='source ~/.bashrc'" >> $HOME/.bash_aliases
+echo "alias e='exit'" >> $HOME/.bash_aliases
+echo "alias dev='cd ~/workspace; pwd; ls -a'" >> $HOME/.bash_aliases
+echo "alias gs='git status'" >> $HOME/.bash_aliases
+echo "alias python='python3'" >> $HOME/.bash_aliases
+echo "alias ptn='python'" >> $HOME/.bash_aliases
+
+# Summary
+me=`basename "$0"`
+echo 'This is the log file echoed from env-setup script ('$0').' >> ~/env-setup.log
+echo 'Custom aliases added into .bash_aliases file.' >> ~/env-setup.log
+echo 'Installed: neofetch for cli system info.' >> ~/env-setup.log
