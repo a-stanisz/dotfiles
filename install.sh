d1c842d8d2b710f127e24a3e8c2aee9b4518bb07
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-17 09:28:27 +0100

    Rearrange lines

--- install.sh
+++ install.sh
@@ -5,14 +5,12 @@ LOG_DIR=''$HOME'/env-setup.log'
 
 # Timestamp:
 echo $(date) >> $LOG_DIR
+echo 'This is the log file echoed from env-setup script ('$0').' >> $LOG_DIR
 
 # Install neofetch, a cli system info tool:
 sudo apt-get install neofetch
+echo 'Installed: neofetch for cli system info.' >> $LOG_DIR
 
 # Symlink .bash_aliases to the home dir:
 ln -sv $HOME/dotfiles/.bash_aliases ~
-
-# Summary:
-echo 'This is the log file echoed from env-setup script ('$0').' >> $LOG_DIR
 echo '.bash_aliases symlinked into home dir.' >> $LOG_DIR
-echo 'Installed: neofetch for cli system info.' >> $LOG_DIR
